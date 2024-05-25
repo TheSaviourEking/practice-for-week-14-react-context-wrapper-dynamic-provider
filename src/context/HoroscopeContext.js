fd453aa86e9696ddf34c2386ee4a3cf64a4da916
TheSaviourEking <saviour.eking@outlook.com>
2024-05-25 03:41:42 +0100

    chore: create a global context

--- src/context/HoroscopeContext.js
+++ src/context/HoroscopeContext.js
@@ -1,3 +1,18 @@
-import { createContext } from 'react';
+import { createContext, useState } from 'react';
+
+import horoscopes from '../data/horoscopes';
 
 export const HoroscopeContext = createContext();
+
+const HoroscopeProvider = (props) => {
+    const [currentSign, setCurrentSign] = useState("Cancer");
+    const sign = horoscopes[currentSign];
+
+    return (
+        <HoroscopeContext.Provider value={{ sign, setCurrentSign }}>
+            {props.children}
+        </HoroscopeContext.Provider>
+    )
+};
+
+export { HoroscopeProvider };
